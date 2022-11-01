17f5883147b239a5d20a5f35be74f7dc8d0e6adc
donpv <donpv@huongvietgroup.com>
2022-11-01 13:58:52 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
-
+// import * as mobilenet from '@tensorflow-models/mobilenet';
+// import * as knnClassifier from '@tensorflow-models/knn-classifier';
+// import {Howl} from 'howler';
+// import soundUrl from './assets/hey_sondn.mp3';
+// var sound = new Howl({
+//   src:[soundUrl]
+// });
+//sound.play();
+import React, {useEffect, useRef} from 'react'; 
 function App() {
+  const video = useRef();
+  const init = async () => {
+    await setupCamera();
+  }
+
+  const setupCamera = () => {
+    return new Promise((resolve, reject) => {
+      navigator.getUseMedia = navigator.getUseMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia;
+      if(navigator.getUseMedia) {
+        navigator.getUseMedia(
+          { video: true },
+          stream => {
+            video.current.srcObject = stream;
+            video.current.addEventListener('loadeddata', resolve);
+          },
+          error => reject(error)
+        );
+      }else {
+        reject();
+      }
+    })
+  } 
+  useEffect(() => {
+    init();
+
+    return () => {
+
+    }
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <video className='video' autoPlay ref={video}>
+
+      </video>
+      <div className='control'>
+        <button className='btn'>Train 1</button>
+        <button className='btn'>Train 2</button>
+        <button className='btn'>Run</button>
+      </div>
     </div>
   );
 }
